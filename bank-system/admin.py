651b84aef407fe3b037b0205e979f19da5c1dca0
valeriawin <baranova.valeriya.by@gmail.com>
2021-10-14 16:53:52 +0300

    refactor: new code style

--- bank-system/admin.py
+++ bank-system/admin.py
@@ -1,31 +1,43 @@
 def rewrite(bank):
     f = open("user.txt", "w")
+
     for elem in bank:
         if elem[-1] != "\n":
             elem = elem + "\n"
+
         f.write(elem)
+        
     f.close()
+
 
 def start_user():
     file1 = open("user.txt", "r")
     bank = []
+
     for line in file1:
         bank.append(line)
+
     file1.close()
+
     return bank
+
 
 def add_user(bank):
     print(bank)
+
     while True:
         temp = input('введите карту или 0 для выхода: ')
         if temp == "0":
             break
         else:
             bank.append(temp)
+
     rewrite(bank)
+
 
 def delete_user(bank):
     print(bank)
+
     while True:
         temp = input('введите карту, которую хотите удалить, или 0 для выхода: ')
         if temp == "0":
@@ -34,4 +46,5 @@ def delete_user(bank):
             for elem in bank:
                 if elem[:len(elem)-1] == temp:
                     bank.remove(elem)
+
     rewrite(bank)
