370dc5fa81da1487a633758b6d2470ca8f063ac2
valeriawin <baranova.valeriya.by@gmail.com>
2021-10-14 16:34:43 +0300

    refactor: new code style

--- TickAir_Progect.py
+++ TickAir_Progect.py
@@ -1,17 +1,15 @@
-depart_pass,arrive_pass,date_pass = map(str,input("Введите место вылета, место прибытия, дату ").split ( ))
-    
-
 def pass_seats():# ввод количества мест
     pass_seats = int(input("введите количество мест "))
     return pass_seats
 
+
 def pass_data():# ввод данных пассажира для заказ билета
-    name, surname, email = map(str,input("Введите Имя Фамилию e-mail ").split ())
+    name, surname, email = map(str,input("Введите Имя, Фамилию, e-mail ").split())
     return name, surname, email
+
 
 class Flight:
     def __init__(self, depart,arrive,date,time,seats,price):
-
         self.depart = depart
         self.arrive = arrive
         self.date = date
@@ -19,11 +17,10 @@ class Flight:
         self.price = price
         self.time = time
 
-    
-
     def ticket_count(self,other): #минусует место от общего количества мест при заказе билета
         self.other = other
         self.seats -= other
+
         return self.seats
 
     def checkin_seats(self):#Проверка на количество мест
@@ -33,20 +30,14 @@ class Flight:
             return False
 
 
-################################################################################################################
-
-minskMoskow = Flight("Минск","Москва","30.04.2021", "13:20", 3, 200) # создаем рейс depart,arrive,date,seats,price
-minskParis = Flight("Минск","Париж","01.05.2021","17:20", 2, 400)# создаем рейс depart,arrive,date,seats,price
-flight_list = [minskMoskow, minskParis]#список всех рейсов
-
-def check_in_fligt():# осуществляет поиск рейса ????как выводить оба результата, разные по времени?????
+def check_in_fligt():  # осуществляет поиск рейса
     for flight in flight_list:
         if depart_pass == flight.depart and arrive_pass == flight.arrive and date_pass == flight.date:
             return flight
                     #f"аэропорт вылета: {flight.depart}, аэропорт прибытия: {flight.arrive}, дата: {flight.date}, время: {flight.time}, стоимость: {flight.price}"
         else: 
             return 0
-#################################################################################################################################
+
 
 def main():
     if check_in_fligt() !=0:
@@ -62,24 +53,14 @@ def main():
         else:
             print(f"Мест недостаточно, осталось {myFlight.seats} места")
 
-    
     else:
         print("По заданному маршруту или в заданную дату полеты не осуществляются")
 
 
+depart_pass, arrive_pass, date_pass = map(str, input("Введите место вылета, место прибытия, дату ").split ( ))
+
+minskMoskow = Flight("Минск","Москва","30.04.2021", "13:20", 3, 200)  # создаем рейс depart,arrive,date,seats,price
+minskParis = Flight("Минск","Париж","01.05.2021","17:20", 2, 400)  # создаем рейс depart,arrive,date,seats,price
+flight_list = [minskMoskow, minskParis]  #список всех рейсов
 
 main()
-
-
-    
-
-    
-
-    
-
-    
-   
-
-
-
-    
