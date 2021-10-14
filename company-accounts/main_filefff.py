f07f1c1d05e11966f25ad8ea9162b3311effdcca
valeriawin <baranova.valeriya.by@gmail.com>
2021-10-14 17:07:34 +0300

    refactor: new code style

--- company-accounts/main_filefff.py
+++ company-accounts/main_filefff.py
@@ -1,10 +1,12 @@
 import openpyxl
-import pretty_errors
 from datetime import datetime
 import time
+
+
 coefficient = {20: 1.9, 15: 1.7, 10: 1.5, 5: 1.3, 1: 1.1}
 slash = "|"
 space = " "
+
 # Excel
 book = openpyxl.open("D:\python_pro\ebook.xlsx", read_only=True)
 sheet = book.active
@@ -13,6 +15,7 @@ sheet = book.active
 #Авторизация или регистрация
 def reg_auto():
     r_a = input("1. Авторизация\n2. Регистрация")
+
     if r_a.isdigit():
         r_a = int(r_a)
         if r_a == 1:
@@ -22,20 +25,26 @@ def reg_auto():
         else:
             print("Неправильный ввод, попробуйте ещё раз")
             reg_auto()
+
     else:
         print("Для ввода необходимо ввести 1 или 2, попробуйте ещё раз")
         reg_auto()
+
 
 # Регистрация
 def registration(login, password):
     file1 = open("D:\python_pro\min_user.txt", "a", encoding = "utf-8")
     file1.write(login + " " + password + " " + "User")
-    return "Регистрация прошла успешно"
     file1.close()
+
+    return "Регистрация прошла успешно"
+
+
 # Авторизация
 def autorization(login, password):
     person  = ""
     file1 = open("D:\python_pro\min_user.txt", "r", encoding = "utf-8")
+
     for line in file1:
         line = line.split()
         if line[0] == login:
@@ -48,16 +57,21 @@ def autorization(login, password):
                     person = "User"
                     print("Вы вошли как User")
                     return person
+
             else:
                 print("Неправильный пароль")
                 autorization(login, password)
+
         else:
             print("Логина не существует")
             autorization(login, password)
+
     file1.close()
+
 
 # Стаж + з/п
 def info():
+
     # Расчёт стажа
     for i in range(2, sheet.max_row + 1):
         fio = sheet[i][0].value
@@ -67,14 +81,16 @@ def info():
         today = datetime.today()
         nyd = datetime(data_year, 1, 1)
         experience = int((today - nyd).days)
+
         if experience >= 365:
             experience = experience // 365
             return experience
+
         else:
             experience = 0
             return experience
-#        salary_month(experience, salary)
-# Расчёт зп
+
+    # Расчёт зп
     def salary_month(experience, salary):
         if experience >= 20:
             salary = salary * coefficient.get(20)
@@ -92,13 +108,15 @@ def info():
             return salary
         else:
             return salary
-# Вывод
+
+        # Вывод
         len_fio = 35 - len(fio)
         len_data = 14 - len(data)
         if len(str(i)) == 1:
             print(i,  fio + (" " * (len_fio)) + "|" + data + (" " * len_data) + "|" + str(salary))
         else:
             print(i, fio + (" " * (len_fio - 1)) + "|" + data + (" " * len_data) + "|" + str(salary))
+
 
 # Для просмотра (User)
 def read():
@@ -108,20 +126,20 @@ def read():
         salary = float(sheet[i][2].value)
         len_fio = 35 - len(fio)
         len_data = 14 - len(data)
-# return
+
         if i == 10:
             return f"{i}. {fio}{space * len_fio}{slash}{data}{space * len_data}{slash}{salary}"
         else:
             return f"{i}. {fio}{space * (len_fio - 1)}{slash}{data}{space * len_data}{slash}{salary}"
 
-########################################################
-
 
 login = input("Введите логин")
 password = input("Введите пароль")
 reg_auto()
+
 # Распределение полномочий
 person = autorization(login, password)
+
 if person == "Admin":
     choice = int(input("\nВыберите действие, которое хотите просмотреть: "
                        "\n\t1. Список работников"
@@ -132,13 +150,16 @@ if person == "Admin":
                        "\n\t6. Все данные"
                        "\n\t7. Внести новые данные"
                        "\n\t0. выход\n"))
+
     while choice != 0:
+
         # 1.Спосок работников
         if choice == 1:
             print("-" * 30)
             for el in range(2, sheet.max_row + 1):
                 fio = sheet[el][0].value
                 print(f"{el - 1}. {fio}")
+
         # 2. Список работников с датой принятия на работу
         elif choice == 2:
             print("-" * 30)
@@ -151,6 +172,7 @@ if person == "Admin":
                     print(f"{el - 1}. {fio}{space * len_fio}{slash} {data} ")
                 else:
                     print(f"{el - 1}. {fio}{space * (len_fio - 1)}{slash} {data} ")
+
         # 3. Список работников и их оклад
         elif choice == 3:
             print("-" * 30)
@@ -163,11 +185,13 @@ if person == "Admin":
                     print(f"{el - 1}. {fio}{space * len_fio}{slash} {salary}")
                 else:
                     print(f"{el - 1}. {fio}{space * (len_fio - 1)}{slash} {salary}")
-        #4. Коэффициены (Зависят от стажа работы)
+
+        #4. Коэффициенты (Зависят от стажа работы)
         elif choice == 4:
             print("-" * 30)
             for key in coefficient:
                print(f"Стаж работы {key} лет и более. Коэффициент - {coefficient[key]}")
+
         # 5. список работников и их итоговая зарплата за месяц"
         elif choice == 5:
             print("-" * 30)
@@ -179,6 +203,7 @@ if person == "Admin":
                     print(f"{el - 1} {fio}{space * len_fio}{slash}{info()}")
                 else:
                     print(f"{el - 1} {fio}{space * (len_fio - 1)}{slash}{info()}")
+
         # 6. Все данные"
         elif choice == 6:
             print("-" * 30)
@@ -192,6 +217,7 @@ if person == "Admin":
                     print(f"{el - 1}. {fio}{space * len_fio} {slash} {data} {slash} {salary} {slash} {info()}")
                 else:
                     print(f"{el - 1}. {fio}{space * (len_fio - 1)} {slash} {data} {slash} {salary} {slash} {info()}")
+
         elif choice == 7:
             fio = input("Введите ФИО")
             data = input("Введите дату проиняти на работу в формате yyyy,mm,dd")
@@ -199,6 +225,7 @@ if person == "Admin":
             i = [(fio, data, salary)]
             sheet.append(i)
             sheet.save("ebook.xlsx")
+
         choice = int(input("\nВыберите действие, которое хотите просмотреть: "
                            "\n\t1. Список работников"
                            "\n\t2. Список работников с датой принятия на работу"
@@ -207,19 +234,9 @@ if person == "Admin":
                            "\n\t5. список работников и их итоговая зарплата за месяц"
                            "\n\t6. Все данные"
                            "\n\t0. выход\n"))
+
 # Просто чтение excel для пользователя
 else:
     print(read())
+
 book.close()
-
-
-
-
-
-
-
-
-
-
-
-
--- company-accounts/main_fileffff.py
+++ company-accounts/main_fileffff.py
@@ -1,22 +1,20 @@
-
-
 import openpyxl
-#import pandas as pd
-import pretty_errors
 from datetime import datetime
-#import time
+
+
 coefficient = {20: 1.9, 15: 1.7, 10: 1.5, 5: 1.3, 1: 1.1}
 slash = "|"
 space = " "
+
 # Excel
 wb = openpyxl.reader.excel.load_workbook(filename = "ebook.xlsx")
-#book = openpyxl.open("D:\python_pro\ebook.xlsx", read_only=True)
 sheet = wb.active
 
 
 #Авторизация или регистрация
 def reg_auto():
     r_a = input("1. Авторизация\n2. Регистрация")
+
     if r_a.isdigit():
         r_a = int(r_a)
         if r_a == 1:
@@ -26,15 +24,18 @@ def reg_auto():
         else:
             print("Неправильный ввод, попробуйте ещё раз")
             reg_auto()
+
     else:
         print("Для ввода необходимо ввести 1 или 2, попробуйте ещё раз")
         reg_auto()
+
 
 # Регистрация
 def registration(login, password):
     file1 = open("D:\python_pro\min_user.txt", "a", encoding = "utf-8")
     file1.write(login + " " + password + " " + "User")
     file1.close()
+
     return "Регистрация прошла успешно"
 
 # Авторизация
@@ -53,13 +54,17 @@ def autorization(login, password):
                     person = "User"
                     print("Вы вошли как User")
                     return person
+
             else:
                 print("Неправильный пароль")
                 autorization(login, password)
+
         else:
             print("Логина не существует")
             autorization(login, password)
+
     file1.close()
+
 
 def salary_month(experience, salary):
     if experience >= 20:
@@ -78,8 +83,11 @@ def salary_month(experience, salary):
         return salary
     else:
         return salary
+
+
 # Стаж + з/п
 def info():
+
     # Расчёт стажа
     for i in range(2, sheet.max_row + 1):
         fio = sheet[i][0].value
@@ -89,22 +97,15 @@ def info():
         today = datetime.today()
         nyd = datetime(data_year, 1, 1)
         experience = int((today - nyd).days)
+
         if experience >= 365:
             experience = experience // 365
             return experience
+
         else:
             experience = 0
             return experience
-        #salary_month(experience, salary)
-# Расчёт зп
 
-# Вывод  return
-        len_fio = 35 - len(fio)
-        len_data = 14 - len(data)
-        if len(str(i)) == 1:
-            print(i,  fio + (" " * (len_fio)) + "|" + data + (" " * len_data) + "|" + str(salary))
-        else:
-            print(i, fio + (" " * (len_fio - 1)) + "|" + data + (" " * len_data) + "|" + str(salary))
 
 # Для просмотра (User)
 def read():
@@ -114,21 +115,20 @@ def read():
         salary = float(sheet[i][2].value)
         len_fio = 35 - len(fio)
         len_data = 14 - len(data)
-# return
+
         if i == 10:
             return f"{i}. {fio}{space * len_fio}{slash}{data}{space * len_data}{slash}{salary}"
         else:
             return f"{i}. {fio}{space * (len_fio - 1)}{slash}{data}{space * len_data}{slash}{salary}"
 
-########################################################
-
-
 
 login = input("Введите логин")
 password = input("Введите пароль")
 reg_auto()
+
 # Распределение полномочий
 person = autorization(login, password)
+
 if person == "Admin":
     choice = int(input("\nВыберите действие, которое хотите просмотреть: "
                        "\n\t1. Список работников"
@@ -138,13 +138,16 @@ if person == "Admin":
                        "\n\t5. список работников и их итоговая зарплата за месяц"
                        "\n\t6. Все данные"
                        "\n\t0. выход\n"))
+
     while choice != 0:
+
         # 1.Список работников
         if choice == 1:
             print("-" * 30)
             for el in range(2, sheet.max_row + 1):
                 fio = sheet[el][0].value
                 print(f"{el - 1}. {fio}")
+
         # 2. Список работников с датой принятия на работу
         elif choice == 2:
             print("-" * 30)
@@ -157,6 +160,7 @@ if person == "Admin":
                     print(f"{el - 1}. {fio}{space * len_fio}{slash} {data} ")
                 else:
                     print(f"{el - 1}. {fio}{space * (len_fio - 1)}{slash} {data} ")
+
         # 3. Список работников и их оклад
         elif choice == 3:
             print("-" * 30)
@@ -169,11 +173,13 @@ if person == "Admin":
                     print(f"{el - 1}. {fio}{space * len_fio}{slash} {salary}")
                 else:
                     print(f"{el - 1}. {fio}{space * (len_fio - 1)}{slash} {salary}")
+
         #4. Коэффициены (Зависят от стажа работы)
         elif choice == 4:
             print("-" * 30)
             for key in coefficient:
                print(f"Стаж работы {key} лет и более. Коэффициент - {coefficient[key]}")
+
         # 5. список работников и их итоговая зарплата за месяц"
         elif choice == 5:
             print("-" * 30)
@@ -185,6 +191,7 @@ if person == "Admin":
                     print(f"{el - 1} {fio}{space * len_fio}{slash}{info()}")
                 else:
                     print(f"{el - 1} {fio}{space * (len_fio - 1)}{slash}{info()}")
+
         # 6. Все данные"
         elif choice == 6:
             print("-" * 30)
@@ -198,15 +205,7 @@ if person == "Admin":
                     print(f"{el - 1}. {fio}{space * len_fio} {slash} {data} {slash} {salary} {slash} {info()}")
                 else:
                     print(f"{el - 1}. {fio}{space * (len_fio - 1)} {slash} {data} {slash} {salary} {slash} {info()}")
-        """elif choice == 7:
-            fio = input("Введите ФИО")
-            data = input("Введите дату проиняти на работу в формате yyyy,mm,dd")
-            salary = input("Введите оклад")
-            up = pd.DataFrame({"ФИО": fio, "Дата": data, "Оклад": salary})
-            up.to_excel("./ebook.xlsx")
-            #i = [(fio, data, salary)]
-            #sheet.append(i)
-            sheet.save("ebook.xlsx")"""
+
         choice = int(input("\nВыберите действие, которое хотите просмотреть: "
                            "\n\t1. Список работников"
                            "\n\t2. Список работников с датой принятия на работу"
@@ -215,18 +214,7 @@ if person == "Admin":
                            "\n\t5. список работников и их итоговая зарплата за месяц"
                            "\n\t6. Все данные"
                            "\n\t0. выход\n"))
+
 # Просто чтение excel для пользователя
 else:
     print(read())
-book.close()
-
-
-
-
-
-
-
-
-
-
-
